b5c41e73b08af48d6f5e56ccceb5f7fbe937aef2
anukulghoshdev <anukulghosh264@gmail.com>
2023-03-13 00:33:25 +0600

    get single product and update a product

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(cors());
 
 const port = process.env.PORT || 5000;
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const { query } = require("express");
 
 //middleware
@@ -18,10 +18,8 @@ app.get("/", async (req, res) => {
   res.send("react state redux server is running");
 });
 
-
 // DB setup
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tr6mdf5.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster3.s7xdhh0.mongodb.net/test`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -52,7 +50,7 @@ client.connect(err => {
 const run = async () => {
   try {
     // db collections->
-    const db = client.db("react-state-redux-2");
+    const db = client.db("learn-redux"); // learn-redux .products
     const productCollection = db.collection("products");
 
     // ROUTES-->
@@ -62,6 +60,51 @@ const run = async () => {
       res.send({ status: true, data: products });
     });
 
+    app.post("/product", async (req, res) => {
+      const product = req.body;
+      const result = await productCollection.insertOne(product);
+      res.send(result);
+    });
+
+    app.delete("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await productCollection.deleteOne({ _id: ObjectId(id) });
+      res.send(result);
+    });
+
+    // app.get("/product", async(req, res)=>{
+    //   const query = req.query.search;
+    //   console.log(query);
+    // })
+
+    // get single product 
+    app.get("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const product = await productCollection.findOne({ _id: ObjectId(id) });
+      res.send({ status: true, data: product });
+    });
+
+    // update a product
+    app.put("/updated-product/:id", async (req, res) => {
+      const product = req.body;
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          model: product.model,
+          brand: product.brand,
+          image: product.image,
+          keyFeature: product.keyFeature,
+          price: product.price,
+          rating: product.rating,
+          spec: product.spec,
+          status: product.status,
+        },
+      };
+      const result = await productCollection.updateOne(filter, updateDoc, options);
+      res.send(result);
+    });
   } finally {
   }
 };
